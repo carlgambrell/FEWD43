65896ed0a6e0ede39d83a23bee20f4e01ab8d172
carlgambrell <carl@carl-gambrells-MacBook-Pro.local>
2015-01-04 19:25:45 -0500

    initial commit

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage();
+});
+
+//* CURRENTLY IN: javascript/main.js */
+
+// $('#menu').multilevelpushmenu({
+// 	menuID: 'test'
+// 	, menuWidth: '20%'
+// 	, collapsed: true
+// 	, containersToPush: [ $( '.full-bleed' ) ],
+// });
+
+ $('.bxwrap').bxSlider({
+ 	auto: true
+});
